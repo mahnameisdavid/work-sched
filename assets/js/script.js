2c65b8dda9b54d7c34922aa95e4b76d9f60c1099
mahnameisdavid <dee.echeverrria@gmail.com>
2022-04-10 21:42:57 -0700

    current day shown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-var saveButton =$(".saveBtn")
+// the variable for the button class will now be switched to javascript
+var saveButton = $(".saveBtn")
+
+//  display the current day in the bottom of the header
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+// this will give the button class
+$(".saveBtn").on("click", function() {
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".col-10").val();
+    console.log("hey")
+    localStorage.setItem(time, plan)
+})
